8e38d2157dde4661c955d70df723715f886f9b9d
Jianmin Chen <59099858+jianmin-chen@users.noreply.github.com>
2024-02-05 21:58:19 -0500

    Update slack.ts

--- lib/slack/slack.ts
+++ lib/slack/slack.ts
@@ -95,22 +95,6 @@ export async function execute(
       await kickoff(props.context.userId)
     }
 
-    // For now, temporarily block if it's not in whitelist
-    if (
-      ![
-        'U03MNFDRSGJ',
-        'UDK5M9Y13',
-        'U032A2PMSE9',
-        'U05TXCSCK7E',
-        'U0C7B14Q3'
-      ].includes(user.slack)
-    )
-      return await props.client.chat.postMessage({
-        channel: user.slack,
-        user: user.slack,
-        text: "You found something... but it's not quite ready yet."
-      })
-
     const permissionLevel = mappedPermissionValues[user.permissions]
     if (!(permissionLevel >= permission))
       return await props.client.chat.postEphemeral({
